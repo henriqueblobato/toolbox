d70b7827269eeff352a4b52fe53867f91b4ad65b
iklob <henrique.cadastro@live.com>
2017-07-15 11:43:53 -0300

    Update message_shuffler.py

--- message_shuffler.py
+++ message_shuffler.py
@@ -3,6 +3,11 @@
 Created on 15 de jul de 2017
 
 @author: henrique
+
+    "Quando lemos uma palavra conhecida, nosso cérebro está na verdade vendo-a como uma
+    imagem única e não um grupo de letras que precisam ser processadas e traduzidas em
+    sequência" O GLOBO, 25/03/2015
+
 '''
 from random import shuffle
 
